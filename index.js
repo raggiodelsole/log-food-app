1503855a0eb938678b9bc3589e50a8d8c3f01319
raggiodelsole <radoslaw.kurowski.94@gmail.com>
2020-08-14 14:13:42 +0200

    adding generator functions

--- index.js
+++ index.js
@@ -16,37 +16,24 @@ readline.on('line', async line => {
     switch (line.trim()) {
         case 'list vegan foods':
             {
-                axios.get(`http://localhost:3001/food`).then(({ data }) => {
+                const { data } = await axios.get(`http://localhost:3001/food`);
+                function* listVeganFood() {
+
                     let idx = 0;
-                    const veganOnly = data.filter(food => {
-                        return food.dietary_preferences.includes('vegan');
-                    })
-                    const veganIterable = {
-                        [Symbol.iterator]() {
-                            return {
-                                [Symbol.iterator]() {
-                                    return this;
-                                },
-                                next() {
-                                    const current = veganOnly[idx];
-                                    idx++;
-                                    if (current) {
-                                        return { value: current, done: false };
-                                    }
-                                    else {
-                                        return { value: current, done: true };
-
-                                    }
-                                },
-                            };
-                        },
-
-                    };
-                    for (let val of veganIterable) {
-                        console.log(val.name);
+                    const veganOnly = data.filter(food =>
+                        food.dietary_preferences.includes('vegan'),
+                    );
+                    while (veganOnly[idx]) {
+                        yield veganOnly[idx];
+                        idx++;
                     }
-                    readline.prompt();
-                });
+                }
+
+                for (let val of listVeganFood()) {
+                    console.log(val.name);
+                }
+                readline.prompt();
+
             }
             break;
         case 'log':
@@ -55,47 +42,24 @@ readline.on('line', async line => {
                 const it = data[Symbol.iterator]();
                 let actionIt;
 
-
-                const actionIterator = {
-
-                    [Symbol.iterator]() {
-                        let positions = [...this.actions];
-                        return {
-                            [Symbol.iterator]() {
-                                return this;
-                            },
-                            next(...args) {
-                                if (positions.length > 0) {
-                                    const position = positions.shift();
-                                    const result = position(...args);
-                                    return { value: result, done: false }
-                                } else {
-                                    return { done: true }
-                                }
-                            },
-                            return() {
-                                positions = [];
-                                return { done: true };
-                            },
-                            throw(error) {
-                                console.log(error);
-                                return { value: undefined, done: true };
-                            }
-                        };
-                    },
-                    actions: [askForServingSize, displayCalories],
-                };
-                function askForServingSize(food) {
+                function askForServingSize() {
                     readline.question(`How many servings did you eat? (as decimal: 1, 0.5, 1.25, etc) `,
                         servingSize => {
                             if (servingSize === 'nevermind' || servingSize === 'n') {
                                 actionIt.return();
 
                             } else {
-                                actionIt.next(servingSize, food);
+                                actionIt.next(servingSize);
                             }
                         },
                     );
+                }
+
+                function* actionGenerator() {
+                    const food = yield;
+                    const servingSize = yield askForServingSize();
+                    yield displayCalories(servingSize, food);
+
                 }
 
                 async function displayCalories(servingSize, food) {
@@ -138,7 +102,8 @@ readline.on('line', async line => {
                         const food = position.value.name;
                         if (food === item) {
                             console.log(`${item} has ${position.value.calories} calories`);
-                            actionIt = actionIterator[Symbol.iterator]();
+                            actionIt = actionGenerator();
+                            actionIt.next();
                             actionIt.next(position.value);
                         }
                         position = it.next();
@@ -146,5 +111,42 @@ readline.on('line', async line => {
                 })
                 break;
             }
+        case `today's log`: {
+            readline.question('Email: ', async emailAdress => {
+                const { data } = await axios.get(
+                    `http://localhost:3001/users?email=${emailAdress}`
+                );
+                const foodLog = data[0].log || [];
+                let totalCalories = 0;
+
+                function* getFoodLog() {
+                    yield* foodLog;
+                }
+
+                for (const entry of getFoodLog()) {
+                    const timestamp = Object.keys(entry)[0];
+                    if (isToday(new Date(Number(timestamp)))) {
+                        console.log(
+                            `${entry[timestamp].food}, ${entry[timestamp].servingSize} serving(s)`
+                        )
+                        totalCalories += entry[timestamp].calories;
+                    }
+                }
+                console.log('-------------------');
+                console.log(`Total Calories: ${totalCalories}`);
+                readline.prompt();
+            })
+            break;
+        }
+            readline.prompt(); //here or maybe after next }
     }
-})
+});
+
+function isToday(timestamp) {
+    const today = new Date();
+    return (
+        timestamp.getDate() === today.getDate() &&
+        timestamp.getMonth() === today.getMonth() &&
+        timestamp.getFullYear() === today.getFullYear()
+    );
+}
